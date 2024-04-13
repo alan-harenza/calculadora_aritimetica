5d8784078f714d5505376e90b2a8fb2d5f10d0ca
nenno04 <alan.harenzap@gmail.com>
2024-04-13 16:44:47 -0300

    fixing the validation step

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,4 @@
-expressions = ["100 - 36", "28 + 18"]
+expressions = ["10000 - 36", "28 + 18"]
 first_number = []
 second_number = []
 operation_row = []
@@ -6,18 +6,18 @@ operation_row = []
 # first step, seeing if the problem conditions fits in the scenarios
 for expression in expressions:
     if len(expressions) > 5:
-        print("too many expressions!")
+        print("Error: Too many problems")
     separation = expression.split()
     # validation of the math operator
     if separation[1] == "/" or expression[1] == "*":
-        print("invalid sign operation")
+        print("Error: Operator must be '+' or '-'.")
         quit()
-    # validation if the first sequence of before the math operator is a number
-    if not separation[0].isnumeric():
-        print("must have only numbers before the sign operation")
+    # validation if the first or second sequence (before and after the math
+    # operator) is a number
+    if not separation[0].isnumeric() or not separation[2].isnumeric():
+        print("Error: Number must only conatain digits.")
         quit()
-    # validation if the second sequence of after the math operator is a number
-    if not separation[2].isnumeric():
-        print("must have only numbers after the sign operation")
+    if len(separation[0]) > 4 or len(separation[2]) > 4:
+        print("Error: Numbers cannot be more than four digits")
         quit()
-# print('validation step is ok to go')
+print('validation step is ok to go')
